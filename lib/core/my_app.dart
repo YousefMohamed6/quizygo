32e04928711331524ac4ae11e189599eba54694f
Yousef Mohamed <yo7sef2002@gmail.com>
2023-11-09 09:48:24 +0200

    add theme

---
+++ lib/core/my_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:quizygo/features/home/presentation/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp._();
+  static const instance = MyApp._();
+  factory MyApp() => instance;
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'َQuizyGo',
+      home: const HomeView(),
+      // applies this theme if the device theme is light mode
+      theme: ThemeData.light(),
+      // applies this theme if the device theme is dark mode
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quizygo/features/home/presentation/home_view.dart';
+import 'package:quizygo/core/my_app.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp._();
-  static const instance = MyApp._();
-  factory MyApp() => instance;
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'َQuizyGo',
-      home: HomeView(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }

d727ab28851f006a666790419ee3774e67f6d4ff
Yousef Mohamed <yo7sef2002@gmail.com>
2023-12-12 13:28:20 +0200

    add assets done

---
+++ lib/core/constants/friends.dart
@@ -0,0 +1,147 @@
+class FriendsAssets {
+  FriendsAssets._();
+  
+  /// Assets for imagesFriendsBohemian
+  /// assats/images/friends/Bohemian.jpg
+  static const String imagesFriendsBohemian = "assats/images/friends/Bohemian.jpg";
+
+  /// Assets for imagesFriendsCamping
+  /// assats/images/friends/Camping.jpg
+  static const String imagesFriendsCamping = "assats/images/friends/Camping.jpg";
+
+  /// Assets for imagesFriendsCandy
+  /// assats/images/friends/Candy.jpg
+  static const String imagesFriendsCandy = "assats/images/friends/Candy.jpg";
+
+  /// Assets for imagesFriendsCasual
+  /// assats/images/friends/Casual.jpg
+  static const String imagesFriendsCasual = "assats/images/friends/Casual.jpg";
+
+  /// Assets for imagesFriendsChips
+  /// assats/images/friends/Chips.jpg
+  static const String imagesFriendsChips = "assats/images/friends/Chips.jpg";
+
+  /// Assets for imagesFriendsChocolate
+  /// assats/images/friends/Chocolate.jpg
+  static const String imagesFriendsChocolate = "assats/images/friends/Chocolate.jpg";
+
+  /// Assets for imagesFriendsClassic
+  /// assats/images/friends/Classic.jpg
+  static const String imagesFriendsClassic = "assats/images/friends/Classic.jpg";
+
+  /// Assets for imagesFriendsCoffee
+  /// assats/images/friends/Coffee.jpg
+  static const String imagesFriendsCoffee = "assats/images/friends/Coffee.jpg";
+
+  /// Assets for imagesFriendsDiving
+  /// assats/images/friends/Diving.jpg
+  static const String imagesFriendsDiving = "assats/images/friends/Diving.jpg";
+
+  /// Assets for imagesFriendsMango
+  /// assats/images/friends/Mango.jpg
+  static const String imagesFriendsMango = "assats/images/friends/Mango.jpg";
+
+  /// Assets for imagesFriendsMatcha
+  /// assats/images/friends/Matcha.jpg
+  static const String imagesFriendsMatcha = "assats/images/friends/Matcha.jpg";
+
+  /// Assets for imagesFriendsMental effort
+  /// assats/images/friends/Mental effort.jpg
+  static const String imagesFriendsMentalEffort = "assats/images/friends/Mental effort.jpg";
+
+  /// Assets for imagesFriendsMilk
+  /// assats/images/friends/Milk.jpg
+  static const String imagesFriendsMilk = "assats/images/friends/Milk.jpg";
+
+  /// Assets for imagesFriendsMountaineering
+  /// assats/images/friends/Mountaineering.jpg
+  static const String imagesFriendsMountaineering = "assats/images/friends/Mountaineering.jpg";
+
+  /// Assets for imagesFriendsNoisy places
+  /// assats/images/friends/Noisy places.jpg
+  static const String imagesFriendsNoisyPlaces = "assats/images/friends/Noisy places.jpg";
+
+  /// Assets for imagesFriendsNuts
+  /// assats/images/friends/Nuts.jpg
+  static const String imagesFriendsNuts = "assats/images/friends/Nuts.jpg";
+
+  /// Assets for imagesFriendsOnline shopping
+  /// assats/images/friends/Online shopping.jpg
+  static const String imagesFriendsOnlineShopping = "assats/images/friends/Online shopping.jpg";
+
+  /// Assets for imagesFriendsPhysical effort
+  /// assats/images/friends/Physical effort.jpg
+  static const String imagesFriendsPhysicalEffort = "assats/images/friends/Physical effort.jpg";
+
+  /// Assets for imagesFriendsPopcorn
+  /// assats/images/friends/Popcorn.jpg
+  static const String imagesFriendsPopcorn = "assats/images/friends/Popcorn.jpg";
+
+  /// Assets for imagesFriendsQuiet places
+  /// assats/images/friends/Quiet places.jpg
+  static const String imagesFriendsQuietPlaces = "assats/images/friends/Quiet places.jpg";
+
+  /// Assets for imagesFriendsReading
+  /// assats/images/friends/Reading.jpg
+  static const String imagesFriendsReading = "assats/images/friends/Reading.jpg";
+
+  /// Assets for imagesFriendsRegular shopping
+  /// assats/images/friends/Regular shopping.jpg
+  static const String imagesFriendsRegularShopping = "assats/images/friends/Regular shopping.jpg";
+
+  /// Assets for imagesFriendsSingle
+  /// assats/images/friends/Single.jpg
+  static const String imagesFriendsSingle = "assats/images/friends/Single.jpg";
+
+  /// Assets for imagesFriendsSky diving
+  /// assats/images/friends/Sky diving.jpg
+  static const String imagesFriendsSkyDiving = "assats/images/friends/Sky diving.jpg";
+
+  /// Assets for imagesFriendsSleeping
+  /// assats/images/friends/Sleeping.jpg
+  static const String imagesFriendsSleeping = "assats/images/friends/Sleeping.jpg";
+
+  /// Assets for imagesFriendsSport
+  /// assats/images/friends/Sport.jpg
+  static const String imagesFriendsSport = "assats/images/friends/Sport.jpg";
+
+  /// Assets for imagesFriendsStrawberry
+  /// assats/images/friends/Strawberry.jpg
+  static const String imagesFriendsStrawberry = "assats/images/friends/Strawberry.jpg";
+
+  /// Assets for imagesFriendsSummer
+  /// assats/images/friends/Summer.jpg
+  static const String imagesFriendsSummer = "assats/images/friends/Summer.jpg";
+
+  /// Assets for imagesFriendsTea
+  /// assats/images/friends/Tea.jpg
+  static const String imagesFriendsTea = "assats/images/friends/Tea.jpg";
+
+  /// Assets for imagesFriendsTrying something new
+  /// assats/images/friends/Trying something new.jpg
+  static const String imagesFriendsTryingSomethingNew = "assats/images/friends/Trying something new.jpg";
+
+  /// Assets for imagesFriendsVanilla
+  /// assats/images/friends/Vanilla.jpg
+  static const String imagesFriendsVanilla = "assats/images/friends/Vanilla.jpg";
+
+  /// Assets for imagesFriendsWatching TV
+  /// assats/images/friends/Watching TV.jpg
+  static const String imagesFriendsWatchingTV = "assats/images/friends/Watching TV.jpg";
+
+  /// Assets for imagesFriendsWinter
+  /// assats/images/friends/Winter.jpg
+  static const String imagesFriendsWinter = "assats/images/friends/Winter.jpg";
+
+  /// Assets for imagesFriendsWith family
+  /// assats/images/friends/With family.jpg
+  static const String imagesFriendsWithFamily = "assats/images/friends/With family.jpg";
+
+  /// Assets for imagesFriendsWith friends
+  /// assats/images/friends/With friends.jpg
+  static const String imagesFriendsWithFriends = "assats/images/friends/With friends.jpg";
+
+  /// Assets for imagesFriendsWith new people
+  /// assats/images/friends/With new people.jpg
+  static const String imagesFriendsWithNewPeople = "assats/images/friends/With new people.jpg";
+}
---
+++ lib/core/constants/partner.dart
@@ -0,0 +1,147 @@
+class PartnerAssets {
+  PartnerAssets._();
+  
+  /// Assets for imagesPartnerAlone
+  /// assats/images/partner/Alone.jpg
+  static const String imagesPartnerAlone = "assats/images/partner/Alone.jpg";
+
+  /// Assets for imagesPartnerArt
+  /// assats/images/partner/Art.jpg
+  static const String imagesPartnerArt = "assats/images/partner/Art.jpg";
+
+  /// Assets for imagesPartnerBaaz
+  /// assats/images/partner/Baaz.jpg
+  static const String imagesPartnerBaaz = "assats/images/partner/Baaz.jpg";
+
+  /// Assets for imagesPartnerBohemian
+  /// assats/images/partner/Bohemian.jpg
+  static const String imagesPartnerBohemian = "assats/images/partner/Bohemian.jpg";
+
+  /// Assets for imagesPartnerCafe
+  /// assats/images/partner/Cafe.jpg
+  static const String imagesPartnerCafe = "assats/images/partner/Cafe.jpg";
+
+  /// Assets for imagesPartnerCasual
+  /// assats/images/partner/Casual.jpg
+  static const String imagesPartnerCasual = "assats/images/partner/Casual.jpg";
+
+  /// Assets for imagesPartnerCat
+  /// assats/images/partner/Cat.jpg
+  static const String imagesPartnerCat = "assats/images/partner/Cat.jpg";
+
+  /// Assets for imagesPartnerClassic
+  /// assats/images/partner/Classic.jpg
+  static const String imagesPartnerClassic = "assats/images/partner/Classic.jpg";
+
+  /// Assets for imagesPartnerDog
+  /// assats/images/partner/Dog.jpg
+  static const String imagesPartnerDog = "assats/images/partner/Dog.jpg";
+
+  /// Assets for imagesPartnerFacebook
+  /// assats/images/partner/Facebook.jpg
+  static const String imagesPartnerFacebook = "assats/images/partner/Facebook.jpg";
+
+  /// Assets for imagesPartnerFancy
+  /// assats/images/partner/Fancy.jpg
+  static const String imagesPartnerFancy = "assats/images/partner/Fancy.jpg";
+
+  /// Assets for imagesPartnerFashion
+  /// assats/images/partner/Fashion.jpg
+  static const String imagesPartnerFashion = "assats/images/partner/Fashion.jpg";
+
+  /// Assets for imagesPartnerFast casual
+  /// assats/images/partner/Fast casual.jpg
+  static const String imagesPartnerFastCasual = "assats/images/partner/Fast casual.jpg";
+
+  /// Assets for imagesPartnerFast food
+  /// assats/images/partner/Fast food.jpg
+  static const String imagesPartnerFastFood = "assats/images/partner/Fast food.jpg";
+
+  /// Assets for imagesPartnerFood truck
+  /// assats/images/partner/Food truck.jpg
+  static const String imagesPartnerFoodTruck = "assats/images/partner/Food truck.jpg";
+
+  /// Assets for imagesPartnerGo out
+  /// assats/images/partner/Go out.jpg
+  static const String imagesPartnerGoOut = "assats/images/partner/Go out.jpg";
+
+  /// Assets for imagesPartnerGym
+  /// assats/images/partner/Gym.jpg
+  static const String imagesPartnerGym = "assats/images/partner/Gym.jpg";
+
+  /// Assets for imagesPartnerHome cooked food
+  /// assats/images/partner/Home cooked food.jpg
+  static const String imagesPartnerHomeCookedFood = "assats/images/partner/Home cooked food.jpg";
+
+  /// Assets for imagesPartnerInstagram
+  /// assats/images/partner/Instagram.jpg
+  static const String imagesPartnerInstagram = "assats/images/partner/Instagram.jpg";
+
+  /// Assets for imagesPartnerJogging
+  /// assats/images/partner/Jogging.jpg
+  static const String imagesPartnerJogging = "assats/images/partner/Jogging.jpg";
+
+  /// Assets for imagesPartnerLong hair
+  /// assats/images/partner/Long hair.jpg
+  static const String imagesPartnerLongHair = "assats/images/partner/Long hair.jpg";
+
+  /// Assets for imagesPartnerMeWe
+  /// assats/images/partner/MeWe.jpg
+  static const String imagesPartnerMeWe = "assats/images/partner/MeWe.jpg";
+
+  /// Assets for imagesPartnerPinterest
+  /// assats/images/partner/Pinterest.jpg
+  static const String imagesPartnerPinterest = "assats/images/partner/Pinterest.jpg";
+
+  /// Assets for imagesPartnerPolitics
+  /// assats/images/partner/Politics.jpg
+  static const String imagesPartnerPolitics = "assats/images/partner/Politics.jpg";
+
+  /// Assets for imagesPartnerShort hair
+  /// assats/images/partner/Short hair.jpg
+  static const String imagesPartnerShortHair = "assats/images/partner/Short hair.jpg";
+
+  /// Assets for imagesPartnerSnapChat
+  /// assats/images/partner/SnapChat.jpg
+  static const String imagesPartnerSnapChat = "assats/images/partner/SnapChat.jpg";
+
+  /// Assets for imagesPartnerSport
+  /// assats/images/partner/Sport.jpg
+  static const String imagesPartnerSport = "assats/images/partner/Sport.jpg";
+
+  /// Assets for imagesPartnerSporty
+  /// assats/images/partner/Sporty.jpg
+  static const String imagesPartnerSporty = "assats/images/partner/Sporty.jpg";
+
+  /// Assets for imagesPartnerStay at home
+  /// assats/images/partner/Stay at home.jpg
+  static const String imagesPartnerStayAtHome = "assats/images/partner/Stay at home.jpg";
+
+  /// Assets for imagesPartnerSunrise
+  /// assats/images/partner/Sunrise.jpg
+  static const String imagesPartnerSunrise = "assats/images/partner/Sunrise.jpg";
+
+  /// Assets for imagesPartnerSunset
+  /// assats/images/partner/Sunset.jpg
+  static const String imagesPartnerSunset = "assats/images/partner/Sunset.jpg";
+
+  /// Assets for imagesPartnerTicTok
+  /// assats/images/partner/TicTok.jpg
+  static const String imagesPartnerTicTok = "assats/images/partner/TicTok.jpg";
+
+  /// Assets for imagesPartnerVK
+  /// assats/images/partner/VK.jpg
+  static const String imagesPartnerVK = "assats/images/partner/VK.jpg";
+
+  /// Assets for imagesPartnerWith friends
+  /// assats/images/partner/With friends.jpg
+  static const String imagesPartnerWithFriends = "assats/images/partner/With friends.jpg";
+
+  /// Assets for imagesPartnerWork
+  /// assats/images/partner/Work.jpg
+  static const String imagesPartnerWork = "assats/images/partner/Work.jpg";
+
+  /// Assets for imagesPartnerX
+  /// assats/images/partner/X.jpg
+  static const String imagesPartnerX = "assats/images/partner/X.jpg";
+}
